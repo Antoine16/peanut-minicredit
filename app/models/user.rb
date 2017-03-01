28c27a0c89170cd722602194ed0d1e77d0907f5c
Pierre-Christophe Callac <pccindo78@msn.com>
2017-03-01 16:42:08 +0100

    add phone and address in device with validate presence true

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   has_many :credits
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :phone, presence: true
+  validates :address, presence: true
 end
