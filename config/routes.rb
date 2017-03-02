e972b0e35e2d8070c8c145eff1db1ced4b39f340
Pierre-Christophe Callac <pccindo78@msn.com>
2017-03-02 12:42:00 +0100

    branch pour tester stripe

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#home'
+
   resources :users do
     resources :credits, only: [ :index, :show, :new, :create ]
+      ressources :payments, only: [ :new, :create ]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
