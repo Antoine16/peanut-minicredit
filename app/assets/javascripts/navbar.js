146d7b0e17f6854871fd39a6cba0f88ae4762207
paul manot <paulmanot@gmail.com>
2017-03-03 12:16:37 +0100

    change btn display to none on page load

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,11 +1,14 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
+$(document).ready(function(){
+  $("#CTA").hide();
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
 
-    if (scroll >= 700) {
-        $("#CTA").show();
-        $(".navbar-wagon").addClass("navbar-wagon-color");
-    } else {
-        $("#CTA").hide();
-        $(".navbar-wagon").removeClass("navbar-wagon-color");
-    }
+      if (scroll >= 700) {
+          $("#CTA").show();
+          $(".navbar-wagon").addClass("navbar-wagon-color");
+      } else {
+          $("#CTA").hide();
+          $(".navbar-wagon").removeClass("navbar-wagon-color");
+      }
+  });
 });
