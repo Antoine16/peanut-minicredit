5936a0f519f418d2135fa09ce70e360a565a8eb2
Antoine V <antoine.veaux@gmail.com>
2017-03-06 11:43:59 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     resources :credits, only: [ :index, :show, :new, :create ]
   end
   get '/sim' => 'pages#sim'
-  get '/credits/payable' => 'credits#payable'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
