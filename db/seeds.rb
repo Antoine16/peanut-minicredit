8597f6e33f51e227890836001a79c153980cab96
jerome homer <homerjerome@gmail.com>
2017-06-06 18:21:23 +0200

    solved rails admin stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,30 +10,3 @@ require 'faker'
 Credit.destroy_all
 User.destroy_all
 
-
-  20.times do
-    new_user = User.new(
-      first_name: Faker::Name.first_name,
-      last_name:  Faker::Name.last_name,
-      address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-      phone:  Faker::PhoneNumber,
-      email:  Faker::Internet.email,
-      password: '123456',
-      )
-      new_user.save!
-
-      20.times do
-        credit = Credit.new(
-        amount_cents: Faker::Number.between(1, 200),
-        state: 'pending',
-        #interest_cents: Faker::Number.between(1, 20),
-        refund_at: Date.today + 1,
-        user: new_user,
-        )
-        credit.save!
-      end
-
-  end
-
-
-
