89f2e86319155cfa11b34b70bb1ad82be4785b1c
jerome homer <homerjerome@gmail.com>
2017-06-24 13:28:15 +0200

    initial Sendgrid smtp configuration

--- app/mailers/credit_mailer.rb
+++
@@ -1,8 +0,0 @@
-class CreditMailer < ApplicationMailer
-
-  def credit_confirmation(credit, user)
-    @user = user
-    @credit = credit
-    mail(to: @user.email, subject: 'Nouveau crédit Peanut!')
-  end
-end
