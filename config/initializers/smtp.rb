d1da767e6f50a61c1669068dad32fd29773f61d0
jerome homer <homerjerome@gmail.com>
2017-06-24 14:27:03 +0200

    changed domain name

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   address: ENV['SENDGRID_SMTP_SERVER'],
   port: '25',
-  domain: 'heroku.com',
+  domain: 'https://peanut-minicredit.herokuapp.com/',
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
   authentication: :plain,
