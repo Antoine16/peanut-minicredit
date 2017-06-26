5e0e1598647bfd1018e4812afd86a781a69ed070
jerome homer <homerjerome@gmail.com>
2017-06-26 15:49:02 +0200

    change smtp port to 2525 as reco by sendgrid

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   address: ENV['SENDGRID_SMTP_SERVER'],
-  port: '25',
+  port: '2525',
   domain: 'https://peanut-minicredit.herokuapp.com/',
   user_name: ENV['SENDGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
