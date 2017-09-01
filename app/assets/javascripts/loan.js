b22a6ed889da3e0c9c8ec63c3263cbb2486f4c68
Antoine Veaux <antoine.veaux@gmail.com>
2017-09-01 21:59:11 +0200

    tauxloan

--- app/assets/javascripts/loan.js
+++ app/assets/javascripts/loan.js
@@ -2,10 +2,10 @@
 $('#loan_capital').keypress(function(){
   setTimeout(function(){
     $('#tranquilité').empty();
-    $('#tranquilité').append(Math.round($('#loan_capital').val() * 1.06, -2));
+    $('#tranquilité').append(Math.round($('#loan_capital').val() * 1.05, -2));
     $('#sécurité').empty();
-    $('#sécurité').append(Math.round($('#loan_capital').val() * 1.03, -2));
+    $('#sécurité').append(Math.round($('#loan_capital').val() * 1.02, -2));
     $('#dynamique').empty();
-    $('#dynamique').append(Math.round($('#loan_capital').val() * 1.09, -2));
+    $('#dynamique').append(Math.round($('#loan_capital').val() * 1.08, -2));
   }, 200);
 })
